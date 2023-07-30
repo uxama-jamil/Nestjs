d065d16e4a148ab8339839140661d8861c4d3994
uxama-jamil <usama_jamil@hotmail.com>
2023-07-30 21:37:28 +0500

    prisma with mysql

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req } from "@nestjs/common";
+import { request } from "express";
+import { AuthService } from "./auth.service";
 
-@Controller()
-export class AuthController{}
+@Controller('auth')
+export class AuthController{
+    constructor(private auth:AuthService){
+
+    }
+    @Post('signin')
+    signIn( @Body() body:any){
+        console.log("body is.....",{dto:body})
+        this.auth.signIn()
+    }
+    @Post('signup')
+    signUp(){
+        this.auth.signUp()
+    }
+    @Get('data')
+    data(){
+        return 'Data is ....'
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from "@nestjs/common";
 
 @Injectable({})
-export class AuthService{}
+export class AuthService{
+    signIn(){
+        return {msg:'message for sign in'}
+    }
+    signUp(){
+        return {msg:'message for sign Un'}
+    }
+}
