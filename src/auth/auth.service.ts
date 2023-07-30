7906fdf018f13a6ae4039a28e6caff5f185f24fd
uxama-jamil <usama_jamil@hotmail.com>
2023-07-30 22:53:55 +0500

    creating entry in db with hashed password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from "@nestjs/common";
 import { AuthDto } from "src/dto";
 import { PrismaService } from "src/prisma/prisma.service";
+import * as argon from 'argon2'
 
 @Injectable({})
 export class AuthService{
     constructor(private prisma:PrismaService){
         
     }
-    signIn(body:AuthDto){
-        return {msg:'message for sign in'}
+    async signIn(body:AuthDto){
+        //generate the password note: argon return promise
+        const hash = await argon.hash(body.password)
+
+        //save the user in the db where we already created user model
+        const user = await this.prisma.user.create({data:{
+            email:body.email,
+            hash
+        }})
+
+        //return the user
+        return user
     }
     signUp(){
         return {msg:'message for sign Un'}
