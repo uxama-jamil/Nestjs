2af7a187c313f871b3e498c21f9185e72f379e7a
uxama-jamil <usama_jamil@hotmail.com>
2023-08-01 20:55:07 +0500

    error handling

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { AuthDto } from "src/dto";
 import { PrismaService } from "src/prisma/prisma.service";
 import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable({})
 export class AuthService{
@@ -9,17 +10,29 @@ export class AuthService{
         
     }
     async signIn(body:AuthDto){
+        try {
+            const hash = await argon.hash(body.password)
+    
+            //save the user in the db where we already created user model
+            const user = await this.prisma.user.create({data:{
+                email:body.email,
+                hash
+            }
+        })
+        delete user.hash
+    
+            //return the user
+            return user
+            
+        } catch (error) {
+            if(error instanceof PrismaClientKnownRequestError){ //error handling from prisma doc
+                if(error.code == 'P2002'){ //P2002 code indicate duplicate entry on enique record, use prisma doc for more ifo
+                    throw new ForbiddenException('Credential taken, duplicate')
+                }
+            }
+            
+        }
         //generate the password note: argon return promise
-        const hash = await argon.hash(body.password)
-
-        //save the user in the db where we already created user model
-        const user = await this.prisma.user.create({data:{
-            email:body.email,
-            hash
-        }})
-
-        //return the user
-        return user
     }
     signUp(){
         return {msg:'message for sign Un'}
